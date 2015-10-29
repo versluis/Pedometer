aacf4fb7517bf8fa642242295decbab4f0f18666
Jay Versluis <jay@versluis.com>
2015-10-29 04:07:44 -0400

    initial commit - concept is working

--- Pedometer/ViewController.m
+++ Pedometer/ViewController.m
@@ -7,21 +7,114 @@
 //
 
 #import "ViewController.h"
+@import CoreMotion;
 
 @interface ViewController ()
-
+@property (strong, nonatomic) CMPedometer *pedometer;
+@property (strong, nonatomic) IBOutlet UILabel *stepsLabel;
+@property (strong, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *cadenceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *paceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *flightsUpLabel;
+@property (strong, nonatomic) IBOutlet UILabel *flightsDownLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self resetLabels];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (CMPedometer *)pedometer {
+    
+    if (!_pedometer) {
+        _pedometer = [[CMPedometer alloc]init];
+    }
+    return _pedometer;
+}
+
+- (IBAction)startTracking:(id)sender {
+    
+    self.stepsLabel.text = @"Gathering data...";
+    
+    // start live tracking
+    [self.pedometer startPedometerUpdatesFromDate:[NSDate date] withHandler:^(CMPedometerData * _Nullable pedometerData, NSError * _Nullable error) {
+        
+        // this block is called for each live update
+        [self updateLabels:pedometerData];
+        
+    }];
+}
+
+- (IBAction)stopTracking:(id)sender {
+    
+    // stop live tracking
+    [self.pedometer stopPedometerUpdates];
+    [self resetLabels];
+}
+
+- (void)resetLabels {
+    
+    // reset labels
+    self.stepsLabel.text = @"Press Start to begin\nPedometer Tracking";
+    self.distanceLabel.text = nil;
+    self.cadenceLabel.text = nil;
+    self.paceLabel.text = nil;
+    self.flightsUpLabel.text = nil;
+    self.flightsDownLabel.text = nil;
+}
+
+- (void)updateLabels:(CMPedometerData *)pedometerData {
+    
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
+    formatter.maximumFractionDigits = 2;
+    
+    // step counting
+    if ([CMPedometer isStepCountingAvailable]) {
+        self.stepsLabel.text = [NSString stringWithFormat:@"Steps: %@", [formatter stringFromNumber:pedometerData.numberOfSteps]];
+    } else {
+        self.stepsLabel.text = @"Step Counter not available.";
+    }
+    
+    // distance
+    if ([CMPedometer isDistanceAvailable]) {
+        self.distanceLabel.text = [NSString stringWithFormat:@"Distance travelled: \n%@ meters", [formatter stringFromNumber:pedometerData.distance]];
+    } else {
+        self.distanceLabel.text = @"Distance estimate not available.";
+    }
+    
+    // pace
+    if ([CMPedometer isPaceAvailable] && pedometerData.currentPace) {
+        self.paceLabel.text = [NSString stringWithFormat:@"Current Pace: \n%@seconds per meter", [formatter stringFromNumber:pedometerData.currentPace]];
+    } else {
+        self.paceLabel.text = @"Pace not available.";
+    }
+    
+    // cadence
+    if ([CMPedometer isCadenceAvailable] && pedometerData.currentCadence) {
+        self.cadenceLabel.text = [NSString stringWithFormat:@"Cadence: \n%@ steps per second", [formatter stringFromNumber: pedometerData.currentCadence]];
+    } else {
+        self.cadenceLabel.text = @"Cadence not available.";
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
 @end
