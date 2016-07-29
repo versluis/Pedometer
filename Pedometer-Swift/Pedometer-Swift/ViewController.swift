605d7a7c97ebffc4ec00f2e0b10eb65dc91ae3ca
Jay Versluis <jay@versluis.com>
2016-07-29 11:58:16 -0400

    updated Swift Project base

---
+++ Pedometer-Swift/Pedometer-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pedometer-Swift
+//
+//  Created by Jay Versluis on 29/07/2016.
+//  Copyright © 2016 Pinkstone Pictures LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pedometer-Swift/Pedometer-SwiftTests/Pedometer_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pedometer_SwiftTests.swift
+//  Pedometer-SwiftTests
+//
+//  Created by Jay Versluis on 29/07/2016.
+//  Copyright © 2016 Pinkstone Pictures LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Pedometer_Swift
+
+class Pedometer_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
